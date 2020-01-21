ec12fd34bacd97ca7ea399dadfe2f48cfbbcb872
n0mn0m <alexander@unexpectedeof.net>
2020-01-20 20:32:48 -0600

    Updated for circuit playground express.
    Updated example with CPX touchpads.

--- keyboard/capcitor_keyboard.py
+++ keyboard/capcitor_keyboard.py
@@ -3,46 +3,39 @@
 
 import time
 
-from adafruit_circuitplayground import cpb
-
-THRESHOLD = 100
-cp.adjust_touch_threshold(THRESHOLD)
+from adafruit_circuitplayground import cp
 
 # Super loop
 while True:
-    if cpb.switch:
-        cpb.pixels[:] = (0, 0, 0)
-        if cpb.touch_A0 > THRESHOLD:
-            cpb.pixels[5] = (255, 0, 0)
-            cpb.pixels[6] = (255, 0, 0)
-            cpb.play_tone(352, 100)
-        elif cpb.touch_A1 > THRESHOLD:
-            cpb.pixels[6] = (200, 128, 0)
-            cpb.pixels[7] = (200, 128, 0)
-            cpb.play_tone(395, 1)
-        if cpb.touch_A2 > THRESHOLD:
-            cpb.pixels[7] = (100, 180, 0)
-            cpb.pixels[8] = (100, 180, 0)
-            cpb.play_tone(418, 1)
-        if cpb.touch_A3 > THRESHOLD:
-            cpb.pixels[8] = (0, 255, 0)
-            cpb.pixels[9] = (0, 255, 0)
-            cpb.play_tone(469, 1)
-        if cpb.touch_A4 > THRESHOLD:
-            cpb.pixels[0] = (0, 180, 128)
-            cpb.pixels[1] = (0, 180, 128)
-            cpb.play_tone(527, 1)
-        if cpb.touch_A5 > THRESHOLD:
-            cpb.pixels[1] = (0, 60, 200)
-            cpb.pixels[2] = (0, 60, 200)
-            cpb.play_tone(558, 1)
-        if cpb.touch_A6 > THRESHOLD:
-            cpb.pixels[2] = (0, 0, 255)
-            cpb.pixels[3] = (0, 0, 255)
-            cpb.play_tone(627, 1)
-        if cpb.touch_A7 > THRESHOLD:
-            cpb.pixels[3] = (150, 0, 180)
-            cpb.pixels[4] = (150, 0, 180)
-            cpb.play_tone(704, 1)
+    if cp.switch:
+        cp.pixels.fill((0, 0, 0))
+        if cp.touch_A1:
+            cp.pixels[5] = (255, 0, 0)
+            cp.pixels[6] = (200, 128, 0)
+            cp.play_tone(395, 1)
+        if cp.touch_A2:
+            cp.pixels[7] = (100, 180, 0)
+            cp.pixels[8] = (100, 180, 0)
+            cp.play_tone(418, 1)
+        if cp.touch_A3:
+            cp.pixels[8] = (0, 255, 0)
+            cp.pixels[9] = (0, 255, 0)
+            cp.play_tone(469, 1)
+        if cp.touch_A4:
+            cp.pixels[0] = (0, 180, 128)
+            cp.pixels[1] = (0, 180, 128)
+            cp.play_tone(527, 1)
+        if cp.touch_A5:
+            cp.pixels[1] = (0, 60, 200)
+            cp.pixels[2] = (0, 60, 200)
+            cp.play_tone(558, 1)
+        if cp.touch_A6:
+            cp.pixels[2] = (0, 0, 255)
+            cp.pixels[3] = (0, 0, 255)
+            cp.play_tone(627, 1)
+        if cp.touch_A7:
+            cp.pixels[3] = (150, 0, 180)
+            cp.pixels[4] = (150, 0, 180)
+            cp.play_tone(704, 1)
     else:
-        cpb.pixels[:] = (0, 0, 0)
+        cp.pixels.fill((0, 0, 0))
